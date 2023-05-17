80ae4cce0f4de284f9796b83ccaedeef3cc48467
A-amirul <amirulislamamir412@gmail.com>
2023-05-17 22:22:44 +0600

    disney dolls paradise client side project setup done

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../pages/Shared/Header';
+import Footer from '../pages/Shared/Footer';
+
+const Main = () => {
+	return (
+		<div>
+			<Header></Header>
+			<div className='min-h-[calc(100vh-137px)]'>
+				<Outlet></Outlet>
+			</div>
+			<Footer></Footer>
+		</div>
+	);
+};
+
+export default Main;
---
+++ src/pages/Shared/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+	return (
+		<div>
+			<h2>this is a header</h2>
+		</div>
+	);
+};
+
+export default Header;
