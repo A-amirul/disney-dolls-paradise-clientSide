2e37e94889588d4a1245edc4c788bf0738180887
A-amirul <amirulislamamir412@gmail.com>
2023-05-18 02:38:38 +0600

    Error page added

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -4,12 +4,14 @@ import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Home/Home/Login";
 import Register from "../pages/Home/Home/Register";
 import Blog from "../pages/Home/Home/Blog";
+import ErrorPage from "../pages/Home/Home/ErrorPage";
 
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Main></Main>,
+		errorElement:<ErrorPage></ErrorPage>,
 		children: [
 			{
 				path: "/",
