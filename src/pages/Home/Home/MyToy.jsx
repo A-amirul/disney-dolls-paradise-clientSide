2e700d0f14ca48bfa5a6e45e8caee2367e265e48
A-amirul <amirulislamamir412@gmail.com>
2023-05-20 21:15:32 +0600

    Delete button done

--- src/pages/Home/Home/MyToy.jsx
+++ src/pages/Home/Home/MyToy.jsx
@@ -7,7 +7,25 @@ import MyToyCard from "./MyToyCard";
 const MyToy = () => {
 	const { user } = useContext(AuthContext);
 	const toys = useLoaderData();
-	const userToys = toys?.filter(toy => user?.email === toy?.email);	
+	const userToys = toys?.filter(toy => user?.email === toy?.email);
+	
+	const handleDelete = id => {
+		const proceed = confirm('Are you sure you want to delete?');
+		if (proceed) {
+			fetch(`http://localhost:4000/alltoys/${id}`, {
+				method: 'DELETE',
+			})
+				.then(res => res.json())
+				.then(data => {
+					console.log(data);
+					if (data.deletedCount > 0) {
+						alert('deleted successful');
+
+						//  const remaining = toys.filter(toy => toy?._id !== id);
+					}
+				})
+		}
+	}
 	return (
 		<div className="md:px-64 px-4 py-8 md:py-16 bg-base-200">
 			<h2 className="text-center font-medium md:text-4xl text-xl">My Toys: {userToys.length}</h2>
@@ -30,6 +48,7 @@ const MyToy = () => {
 							userToys?.map(myToy => <MyToyCard
 								key={myToy._id}
 								myToy={myToy}
+								handleDelete={handleDelete}
 							></MyToyCard>)
 						}
 					</tbody>
--- src/pages/Home/Home/MyToyCard.jsx
+++ src/pages/Home/Home/MyToyCard.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 
-const MyToyCard = ({ myToy }) => {
-	const { seller, toyName, sub_category, price, quantity,_id } = myToy;
+const MyToyCard = ({ myToy, handleDelete }) => {
+	const { seller, toyName, sub_category, price, quantity, _id } = myToy;
+	
+	
 	
 	return (
 		
@@ -16,7 +18,7 @@ const MyToyCard = ({ myToy }) => {
 			<td>
 				<Link to={`/updateToy/${_id}`}> <button className=" bg-blue-600 text-white hover:bg-blue-800 px-8 py-2 mx-4 rounded-md transition">Edit</button> </Link>
 		
-				<button className=" bg-blue-600 text-white hover:bg-blue-800 px-4 py-2 rounded-md transition">Delete</button>
+				<button onClick={()=>handleDelete(_id)} className=" bg-blue-600 text-white hover:bg-blue-800 px-4 py-2 rounded-md transition">Delete</button>
 			</td>
 		</tr>
 	);
